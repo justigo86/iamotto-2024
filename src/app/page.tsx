4396a1226d2a3c04a7b0b5f4d815fd0a6f6da715
Otto <justigo86@gmail.com>
2024-07-08 00:49:33 -0400

    updated navbar location to be persistent (layout.tsx) and implement redirect for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <main className="flex flex-row min-h-screen items-center justify-between">
-      <Navbar />
       <Hero />
     </main>
   );
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { motion, Variants } from "framer-motion";
+
+type Props = {};
+
+const navMenuVariants: Variants = {
+  hidden: { height: 0, transition: { duration: 0.5 } },
+  visible: {
+    height: "100vh",
+    transition: { duration: 0.8, ease: "circInOut" },
+  },
+};
+
+const ProjectsPage = (props: Props) => {
+  return (
+    <motion.div
+      className="bg-light-chestnutRose dark:bg-dark-thunderbird h-screen w-screen overflow-hidden"
+      variants={navMenuVariants}
+      initial="visible"
+    >
+      Projects
+    </motion.div>
+  );
+};
+
+export default ProjectsPage;
