ff45253d1fb79ad0e6dd8cc438701ab82b2d1691
Otto <justigo86@gmail.com>
2024-07-10 00:54:42 -0400

    now looping, but not syncing on loop, overlay covers navmenu, and no mouseover effects with ticker yet

--- src/components/hero/ticker.tsx
+++ src/components/hero/ticker.tsx
@@ -23,6 +23,36 @@ const marqueeVariants = {
 };
 
 const Ticker = (props: Props) => {
+  const [numDupes, setNumDupes] = React.useState<number>(1);
+  const tickerRef = React.useRef<HTMLDivElement>(null);
+  const [tickerContentWidth, setTickerContentWidth] = React.useState<
+    number | null
+  >(0);
+
+  React.useEffect(() => {
+    let contentWidth = 0;
+
+    for (let index = 0; index < cards.length; index++) {
+      const element = document.getElementById("_" + index)?.clientWidth;
+      if (element) {
+        contentWidth += element;
+      }
+    }
+
+    setTickerContentWidth(contentWidth);
+  }, []);
+
+  React.useEffect(() => {
+    if (tickerRef.current && tickerContentWidth) {
+      setNumDupes(
+        Math.max(
+          Math.ceil((2 * tickerRef.current.clientWidth) / tickerContentWidth),
+          1
+        )
+      );
+    }
+  }, [tickerRef.current, tickerContentWidth]);
+
   return (
     <section className="relative h-[30vh] w-screen bg-white flex items-center">
       <div className="h-[25vh] w-screen bg-pink-500 flex items-center overflow-hidden">
@@ -34,6 +64,11 @@ const Ticker = (props: Props) => {
           {cards.map((card) => {
             return <Card key={card.id} card={card} />;
           })}
+          {[...Array(numDupes)].map((_) =>
+            cards.map((card) => {
+              return <Card key={card.id} card={card} />;
+            })
+          )}
         </motion.div>
       </div>
     </section>
@@ -52,9 +87,9 @@ const Card = ({ card }: { card: CardType }) => {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+        className="absolute inset-0 transition-transform duration-300 group-hover:scale-110"
       ></div>
-      <div className="absolute inset-0 z-10 grid place-content-center">
+      <div className="absolute inset-0 grid place-content-center">
         <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
           {card.id}
         </p>
