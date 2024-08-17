1c3e006e0862cd5828eed86b88198d24f261063a
Otto <justigo86@gmail.com>
2024-08-16 22:28:46 -0400

    continuing to work on mouse-tracking in hero - slight update to connect page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 const Connect = (props: Props) => {
   return (
-    <div className="bg-blue-400 dark:bg-blue-900 h-screen w-screen overflow-hidden flex justify-center items-center">
-      Connect
+    <div className="bg-purple-300 dark:bg-purple-800 h-screen w-screen overflow-hidden flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-raleway font-bold">Connect</h1>
     </div>
   );
 };
--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -13,22 +13,51 @@ import HeroIcons from "../../components/hero/heroIcons";
 
 const Hero = () => {
   // const mouse = React.useMemo(() => ({ x: 0, y: 0 }), []);
-  const [mouse, setMouse] = React.useState({ x: 0, y: 0 });
-  React.useEffect(() => {
-    const mouseMovement = (e: { clientX: number; clientY: number }) => {
-      setMouse({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener("mousemove", mouseMovement);
+  // const [mouse, setMouse] = React.useState({ x: 0, y: 0 });
 
-    return () => {
-      window.removeEventListener("mousemove", mouseMovement);
-    };
-  }, [mouse]);
+  const innerMovement = (
+    mouseX: number,
+    mouseY: number,
+    boundX: number,
+    boundY: number
+  ): number => {
+    const difX = mouseX - boundX;
+    const difY = mouseY - boundY;
+    return (Math.atan2(difY, difX) * 180) / Math.PI;
+  };
+  React.useEffect(() => {
+    const container = document.getElementById("container");
+    const inner = document.getElementById("inner");
+    const bound = container?.getBoundingClientRect();
+    let containerX: number;
+    let containerY: number;
+
+    window.addEventListener("mousemove", (e) => {
+      if (!bound || !inner) return;
+      containerX = bound?.left + bound?.width / 2;
+      containerY = bound?.top + bound?.height / 2;
+      const mvmt = innerMovement(e.clientX, e.clientY, containerX, containerY);
+      inner.style.transform = `rotate(${90 + mvmt}deg)`;
+    });
+
+    // return () => {
+    //   window.removeEventListener("mousemove", innerMovement);
+    // };
+  }, []);
   return (
     <div className="flex justify-center w-screen max-w-7xl">
       <div className="flex justify-between w-full">
         <div className="text-3xl">
-          <p>{mouse.x}</p> <p>{mouse.y}</p>
+          {/* <p>{mouse.x}</p> <p>{mouse.y}</p> */}
+          <div id="container" className="w-20 h-20 bg-white rounded-full">
+            <div
+              id="inner"
+              className="w-5 h-5 bg-black rounded-full"
+              // style={{
+              //   left: `${mouse.x - }`,
+              // }}
+            ></div>
+          </div>
         </div>
         <div className="flex flex-col gap-4 items-end">
           {/* <div
