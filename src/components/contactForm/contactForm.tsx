8cd370178034dcc2927ff21fed416fd43772575f
Otto <justigo86@gmail.com>
2024-12-02 00:36:29 -0500

    initial setup to implement emailJS with contact form

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { toast } from "sonner";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -15,6 +14,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { BaseSyntheticEvent, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   name: z.string().min(0).max(50),
@@ -22,24 +24,63 @@ const formSchema = z.object({
   comment: z.string().optional(),
 });
 
-export default function MyForm() {
+export default function ContactForm() {
+  const [formInput, setFormInput] = useState({
+    name: "",
+    email: "",
+    comment: "",
+  });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (
+    data: { name: string; email: string; comment?: string | undefined },
+    e?: BaseSyntheticEvent<object, any, any>
+  ) => {
+    e?.preventDefault();
+
+    const emailjsServiceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
+    const emailjsTemplateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+    const emailjsUserId = process.env.NEXT_PUBLIC_EMAILJS_USER_ID!;
     try {
-      console.log(values);
-      toast(
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-        </pre>
+      const res = await emailjs.send(
+        emailjsServiceId,
+        emailjsTemplateId,
+        formInput,
+        emailjsUserId
       );
+
+      if (res.status === 200) {
+        console.log("Email sent", res);
+        toast.success("Email sent successfully!");
+        // reset form
+        setFormInput({
+          name: "",
+          email: "",
+          comment: "",
+        });
+      }
     } catch (error) {
-      console.error("Form submission error", error);
-      toast.error("Failed to submit the form. Please try again.");
+      console.error("Error sending email", error);
+      toast.error("Failed to send email. Please try again.");
     }
-  }
+  };
+
+  // function onSubmit(values: z.infer<typeof formSchema>) {
+  //   try {
+  //     console.log(values);
+  //     toast(
+  //       <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+  //         <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+  //       </pre>
+  //     );
+  //   } catch (error) {
+  //     console.error("Form submission error", error);
+  //     toast.error("Failed to submit the form. Please try again.");
+  //   }
+  // }
 
   return (
     <Form {...form}>
