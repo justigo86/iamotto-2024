3f5283927dfb88c76089012eda76e52a5538317f
Otto <justigo86@gmail.com>
2024-12-04 18:07:18 -0500

    made some adjustments to get the form up and running. Confirmed email receipt for recipient.

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -14,57 +14,67 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { BaseSyntheticEvent, useState } from "react";
+import { useState } from "react";
 import emailjs from "@emailjs/browser";
 import { toast } from "sonner";
 
 const formSchema = z.object({
   name: z.string().min(0).max(50),
   email: z.string(),
-  comment: z.string().optional(),
+  comments: z.string().optional(),
 });
 
 export default function ContactForm() {
-  const [formInput, setFormInput] = useState({
-    name: "",
-    email: "",
-    comment: "",
-  });
+  // const [formInput, setFormInput] = useState({
+  //   name: "",
+  //   email: "",
+  //   comment: "",
+  // });
+  const [response, setResponse] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
-  const onSubmit = async (
-    data: { name: string; email: string; comment?: string | undefined },
-    e?: BaseSyntheticEvent<object, any, any>
-  ) => {
-    e?.preventDefault();
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
     const emailjsServiceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
     const emailjsTemplateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
-    const emailjsUserId = process.env.NEXT_PUBLIC_EMAILJS_USER_ID!;
-    try {
-      const res = await emailjs.send(
-        emailjsServiceId,
-        emailjsTemplateId,
-        formInput,
-        emailjsUserId
-      );
+    const emailjsUserId = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
 
-      if (res.status === 200) {
-        console.log("Email sent", res);
-        toast.success("Email sent successfully!");
-        // reset form
-        setFormInput({
-          name: "",
-          email: "",
-          comment: "",
-        });
-      }
+    try {
+      emailjs
+        .sendForm(
+          emailjsServiceId,
+          emailjsTemplateId,
+          e.currentTarget,
+          emailjsUserId
+        )
+        .then(
+          (res) => {
+            if (res.status === 200) {
+              console.log("Email sent", res);
+              toast.success("Email sent successfully!");
+              setResponse(true);
+            } else {
+              toast.error("Email failed to send. Please try again.");
+              console.log("Email failed to send.", res);
+            }
+          },
+          (err) => {
+            console.log(err, "Email failed to send.");
+          }
+        );
     } catch (error) {
       console.error("Error sending email", error);
       toast.error("Failed to send email. Please try again.");
+    }
+  };
+
+  const clearResponse = () => {
+    if (response === true) {
+      setResponse(false);
     }
   };
 
@@ -85,14 +95,16 @@ export default function ContactForm() {
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="max-w-full min-w-80 border-slate-700"
+        onSubmit={onSubmit}
+        onClick={clearResponse}
+        className="max-w-full min-w-80 md:min-w-96"
       >
         <FormField
           control={form.control}
+          defaultValue=""
           name="name"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="my-4">
               <FormLabel>Name:</FormLabel>
               <FormControl>
                 <Input placeholder="Enter your name" type="text" {...field} />
@@ -105,9 +117,10 @@ export default function ContactForm() {
 
         <FormField
           control={form.control}
+          defaultValue=""
           name="email"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="my-4">
               <FormLabel>Email:</FormLabel>
               <FormControl>
                 <Input
@@ -124,9 +137,10 @@ export default function ContactForm() {
 
         <FormField
           control={form.control}
-          name="comment"
+          defaultValue=""
+          name="comments"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="my-4">
               <FormLabel>Comment/Message</FormLabel>
               <FormControl>
                 <Textarea
