a26e88839a9bf5af9d644b9871f3f41bea9e7dfd
Otto <justigo86@gmail.com>
2024-12-10 18:50:12 -0500

    updated h1 spacing on each section and mapping over experiences

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -4,7 +4,9 @@ const Connect = () => {
   return (
     <div className="bg-purple-300 dark:bg-purple-800 w-screen min-h-screen h-content overflow-y-visible md:overflow-hidden pt-32">
       <main className="container flex flex-col justify-center items-center mx-auto p-4 max-w-4xl">
-        <h1 className="text-5xl font-urbanist font-bold">Connect</h1>
+        <h1 className="text-5xl font-urbanist font-bold mb-8 md:mb-[3rem]">
+          Connect
+        </h1>
         <ContactForm />
       </main>
     </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,41 +12,43 @@ const Projects = () => {
   ];
 
   return (
-    <div className="flex flex-col container mx-auto px-4 py-12">
-      <h1 className="self-center text-5xl font-urbanist font-bold mb-10 md:mb-[4rem]">
-        Projects
-      </h1>
-      {rows.map((row, rowIndex) => (
-        <div key={rowIndex} className="relative flex mb-10 md:mb-24">
-          <div
-            className={`flex flex-col md:flex-row gap-10 w-full md:w-4/5 mx-auto md:${
-              rowIndex % 2 === 0 ? "ml-4" : "mr-4 justify-end"
-            }`}
-          >
-            {row.map((project, index) => (
-              <div
-                key={project.id}
-                className={`flex  ${
-                  rowIndex % 2 === 0
-                    ? index % 2 === 0
-                      ? "justify-end"
-                      : "justify-start"
-                    : index % 2 === 0
-                    ? "justify-start"
-                    : "justify-end"
-                }`}
-              >
-                <ProjectCard project={project} index={index} />
-              </div>
-            ))}
-          </div>
-          {/* {rowIndex < rows.length - 1 && (
+    <div className="bg-orange-200 dark:bg-orange-600 w-screen min-h-screen h-content overflow-y-visible md:overflow-hidden pt-32">
+      <main className="flex flex-col container mx-auto px-4 py-12">
+        <h1 className="self-center text-5xl font-urbanist font-bold mb-10 md:mb-[4rem]">
+          Projects
+        </h1>
+        {rows.map((row, rowIndex) => (
+          <div key={rowIndex} className="relative flex mb-10 md:mb-24">
+            <div
+              className={`flex flex-col md:flex-row gap-10 w-full md:w-4/5 mx-auto md:${
+                rowIndex % 2 === 0 ? "ml-4" : "mr-4 justify-end"
+              }`}
+            >
+              {row.map((project, index) => (
+                <div
+                  key={project.id}
+                  className={`flex  ${
+                    rowIndex % 2 === 0
+                      ? index % 2 === 0
+                        ? "justify-end"
+                        : "justify-start"
+                      : index % 2 === 0
+                      ? "justify-start"
+                      : "justify-end"
+                  }`}
+                >
+                  <ProjectCard project={project} index={index} />
+                </div>
+              ))}
+            </div>
+            {/* {rowIndex < rows.length - 1 && (
             <div className="hidden md:block">
               <Arrow direction={rowIndex % 2 === 0 ? 'right' : 'left'} />
             </div>
           )} */}
-        </div>
-      ))}
+          </div>
+        ))}
+      </main>
     </div>
   );
 };
